cb5a70215910d47ce49ca5a53bef998effe7054e
LuvArora-coder <luv.a@somaiya.edu>
2022-04-09 23:04:09 +0530

    fixes

--- audisl/urls.py
+++ audisl/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('about/',views.about_view,name='about'),
-    path('contact/',views.contact_view,name='contact'),
-    path('login/',views.login_view,name='login'),
-    path('logout/',views.logout_view,name='logout'),
-    path('signup/',views.signup_view,name='signup'),
-    path('animation/',views.animation_view,name='animation'),
-    path('',views.home_view,name='home'),
-    path('animation/',views.animation_view,name='animation')
+    #path('admin/', admin.site.urls),
+    path('about/', views.about_view, name='about'),
+    path('contact/', views.contact_view, name='contact'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup_view, name='signup'),
+    path('animation/', views.animation_view, name='animation'),
+    path('', views.home_view, name='home'),
+    path('animation/', views.animation_view, name='animation')
 ]
