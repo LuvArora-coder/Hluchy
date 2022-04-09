886325eb18c24a496d2842e317feff13e3befb82
tanmay <tanmay.tete@sommaiya.edu>
2022-04-09 22:55:08 +0530

    some more changes

--- Hluchy/settings.py
+++ Hluchy/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'audisl',
-    'videoconf',
 ]
 
 MIDDLEWARE = [
@@ -108,13 +107,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
-<<<<<<< HEAD
-MEDIA_DIRS = [ BASE_DIR /'media',]
-=======
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
->>>>>>> 6f3abe30d979889e6e1c7e94b32c2fd3c3a29e08
 MEDIA_URL = '/media/'
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- Hluchy/urls.py
+++ Hluchy/urls.py
@@ -8,5 +8,4 @@ from audisl import views as aud_view
 urlpatterns = [
     path('', include('audisl.urls')),
     path('admin/', admin.site.urls),
-    path('save_audio/', aud_view.ajax, name="save_audio"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
