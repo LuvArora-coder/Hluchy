935f5c5abff018debc30582392e84b34af406759
tanmay <tanmay.tete@sommaiya.edu>
2022-04-09 13:56:22 +0530

    minor

--- videoconf/views.py
+++ videoconf/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def audiotogesture(request):
+    return render(request,'audio_to_image.html')
